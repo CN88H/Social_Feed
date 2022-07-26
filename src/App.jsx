753f2fc2b883ff515b9c984b979397547e096e18
CN88H <cheenou88@gmail.com>
2022-07-26 14:30:08 -0500

    Able to add new Post to App.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import React, { useState } from 'react';
 import DisplayPosts from './Components/DisplayPosts/DisplayPosts';
+import CreatePost from './Components/CreatePost/CreatePost';
 
 
 function App() {
 
   const [entries, setEntries] = useState([{name: 'Chee', post: 'Hello World!'}, {name: 'Clay', post: 'Goodbye World!'}])
 
+  function addNewPost(entry){
+    let tempPost = [entry, ...entries];
+
+    setEntries(tempPost);
+  }
+
   return (
     <div>
       <h3>SocialFeed</h3>
+      <CreatePost addNewPostProp={addNewPost}/>
       <DisplayPosts parentEntries={entries} />
 
     </div>
--- src/Components/DisplayPosts/DisplayPosts.jsx
+++ src/Components/DisplayPosts/DisplayPosts.jsx
@@ -3,8 +3,8 @@ const DisplayPosts = (props) => {
     return ( 
         <table>
             <thead>
-            <tr>Name</tr>
-            <tr>Post</tr>
+            {/* <tr>Name</tr>
+            <tr>Post</tr> */}
 
             </thead>
             <tbody>
